8cccf0dc84dabab5b1fca7cc75c0c128d5644661
Huyentram27 <89881902+Huyentram27@users.noreply.github.com>
2022-02-09 03:18:16 -0700

    Update StorageMap.java

--- tram/StorageMap.java
+++ tram/StorageMap.java
@@ -8,23 +8,25 @@ public class StorageMap {
 	
 	public static void main(String args[])
     {
-	QueDB data = new QueDB();
+		
 	HashMap<String, Double> storageProduct = new HashMap<>();
 	HashMap<Integer, String> storageSupplier = new HashMap<>();
 
-    // Add keys and values (supplierId, productId)
+   
 	Supplier supplier = new Supplier();
 	
 	String productId = supplier.getProductId();
 	Double quantity = supplier.getQuantity();
 	Double wholesale = supplier.getWholesale();
 	Double saleprice = supplier.getSalePrice();
-    Integer supplierId = supplier.getSupplierId();
+    	Integer supplierId = supplier.getSupplierId();
     
-    
+    //Add key and value (productId, attribute of product)
 	storageProduct.put(productId, quantity);
 	storageProduct.put(productId, wholesale);
 	storageProduct.put(productId, saleprice);
+	
+	 // Add keys and values (supplierId, productId)
 	storageSupplier.put(supplierId,productId);
 	
 	
