4ad2f4ac93d1845af079b24fa2a24ad24e588e2a
Huyentram27 <89881902+Huyentram27@users.noreply.github.com>
2022-02-19 15:49:00 -0700

    Add files via upload

---
+++ Main Folder for Database/mySql/Sprint1Database.sql
@@ -0,0 +1,15 @@
+CREATE database Sprint1;
+use Sprint1;
+CREATE table productInfo(ProductID CHAR(12),
+						 Quantity double,
+                         Wholesale double,
+                         Saleprice double,
+                         SupplierID CHAR(8));
+select * FROM productInfo;
+use Sprint1;
+CREATE table userInfo(userName CHAR(18),
+					  pass CHAR(18)),
+                      authorization double;
+			
+select * FROM userInfo;
+drop table userInfo;
