7c3b4e4d111a943e95a3b5edd8407f2633558527
Huyentram27 <89881902+Huyentram27@users.noreply.github.com>
2022-02-23 17:36:43 -0700

    Update Sprint1Database.sql
    change   column for authorization in table for userInfo  from accepting a number to accepting a string.

--- Main Folder for Database/mySql/Sprint1Database.sql
+++ Main Folder for Database/mySql/Sprint1Database.sql
@@ -1,15 +1,17 @@
 CREATE database Sprint1;
-use Sprint1;
+
+use sprint1;
 CREATE table productInfo(ProductID CHAR(12),
 						 Quantity double,
                          Wholesale double,
                          Saleprice double,
                          SupplierID CHAR(8));
 select * FROM productInfo;
+
 use Sprint1;
 CREATE table userInfo(userName CHAR(18),
-					  pass CHAR(18)),
-                      authorization double;
+			pass CHAR(18),
+                      authorization CHAR (2));
 			
+
 select * FROM userInfo;
-drop table userInfo;
