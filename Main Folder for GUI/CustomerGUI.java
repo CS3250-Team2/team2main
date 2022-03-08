6cdee86e3e2017b4e92f28fe819d18afece207cb
uoods <98117453+uoods@users.noreply.github.com>
2022-03-08 11:42:29 -0700

    updating customerGUI with the current build

--- Main Folder for GUI/CustomerGUI.java
+++ Main Folder for GUI/CustomerGUI.java
@@ -1,33 +1,35 @@
 import java.awt.EventQueue;
-import java.awt.FlowLayout;
-import java.awt.LayoutManager;
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.util.List;
-
-import javax.swing.JFrame;
-import javax.swing.JTable;
-import javax.swing.JScrollPane;
-import javax.swing.JButton;
-import javax.swing.JTextField;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-
-import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
 
 public class CustomerGUI {
 	
 	
-
+	
 	private JFrame frmClass;
 	private JTable table;
 	private ProductTable productTable;
+	private OrderTable orderTable;
 	/**
 	 * Launch the application.
 	 */
-//	public static void main(String[] args) 
+
 	public static void NewScreen() {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -45,18 +47,16 @@ public class CustomerGUI {
 	private JButton searchBtn;
 	private JButton showBtn;
 	private JButton addBtn;
-	private JButton deleteBtn;
-	private JButton editBtn;
-	private JTextField supplierIDtextField;
 	private JLabel prodIdLabel;
-	private JTextField productIDtextField ;
+	private JTextField ProductIDtextField ;
 	private JTextField quantitytextField;
 	private JLabel quantitiyLabel;
-	private JLabel wholesaleLabel;
-	private JTextField wholesaletextField;
-	private JLabel salepriceLabel;
-	private JTextField 	salepricetextField;
-	private JLabel suplierIdLabel;
+	private JLabel custEmailLabel;
+	private JTextField custEmailtextfield;
+	private JLabel custLocationLabel;
+	private JTextField 	custLocationtextField;
+	private JLabel dateLabel;
+	private JTextField datetextField;
 	private JButton exitDbBtn;
 	String authorization; 
 	
@@ -66,6 +66,7 @@ public class CustomerGUI {
 	public CustomerGUI() {
 		try {
 			productTable = new ProductTable();
+			orderTable = new OrderTable();
 		}
 		catch (Exception exc) {       //to check if the db connection was successful or not
 	        System.out.println("Oops, error!");
@@ -101,46 +102,46 @@ public class CustomerGUI {
 		prodIdLabel.setBounds(6, 12, 95, 16);
 		frmClass.getContentPane().add(prodIdLabel);
 	
-		productIDtextField = new JTextField();
-		productIDtextField.setBounds(6, 40, 130, 26);
-		frmClass.getContentPane().add(productIDtextField);
-		productIDtextField.setColumns(10);
+		ProductIDtextField = new JTextField();
+		ProductIDtextField.setBounds(6, 40, 130, 26);
+		frmClass.getContentPane().add(ProductIDtextField);
+		ProductIDtextField.setColumns(10);
 		
 		quantitytextField = new JTextField();
 		quantitytextField.setBounds(148, 40, 95, 26);
 		frmClass.getContentPane().add(quantitytextField );
 		quantitytextField.setColumns(10);
 		
-		wholesaletextField = new JTextField();
-		wholesaletextField.setBounds(255, 40, 95, 26);
-		frmClass.getContentPane().add(wholesaletextField);
-		wholesaletextField.setColumns(10);
+		custEmailtextfield = new JTextField();
+		custEmailtextfield.setBounds(255, 40, 95, 26);
+		frmClass.getContentPane().add(custEmailtextfield);
+		custEmailtextfield.setColumns(10);
 		
 		quantitiyLabel = new JLabel("Quantity ");
 		quantitiyLabel.setBounds(148, 12, 61, 16);
 		frmClass.getContentPane().add(quantitiyLabel);
 		
-		wholesaleLabel = new JLabel("Wholesale");
-		wholesaleLabel.setBounds(255, 12, 84, 16);
-		frmClass.getContentPane().add(wholesaleLabel);
+		custEmailLabel = new JLabel("Email");
+		custEmailLabel.setBounds(255, 12, 84, 16);
+		frmClass.getContentPane().add(custEmailLabel);
 		
-		salepricetextField = new JTextField();
-		salepricetextField.setBounds(362, 40, 95, 26);
-		frmClass.getContentPane().add(salepricetextField);
-		salepricetextField.setColumns(10);
+		custLocationtextField = new JTextField();
+		custLocationtextField.setBounds(362, 40, 95, 26);
+		frmClass.getContentPane().add(custLocationtextField);
+		custLocationtextField.setColumns(10);
 		
-		salepriceLabel = new JLabel("Saleprice");
-		salepriceLabel.setBounds(362, 12, 61, 16);
-		frmClass.getContentPane().add(salepriceLabel);
+		custLocationLabel = new JLabel("Location");
+		custLocationLabel.setBounds(362, 12, 61, 16);
+		frmClass.getContentPane().add(custLocationLabel);
 		
-		supplierIDtextField = new JTextField();
-		supplierIDtextField.setBounds(469, 40, 130, 26);
-		frmClass.getContentPane().add(supplierIDtextField);
-		supplierIDtextField.setColumns(10);
+		datetextField = new JTextField();
+		datetextField.setBounds(469, 40, 130, 26);
+		frmClass.getContentPane().add(datetextField);
+		datetextField.setColumns(10);
 		
-		suplierIdLabel = new JLabel("Supplier ID");
-		suplierIdLabel.setBounds(469, 12, 95, 16);
-		frmClass.getContentPane().add(suplierIdLabel);
+		dateLabel = new JLabel("Date");
+		dateLabel.setBounds(469, 12, 95, 16);
+		frmClass.getContentPane().add(dateLabel);
 		
 		exitDbBtn = new JButton("Exit Database");
 		exitDbBtn.setBounds(6, 283, 134, 29);
@@ -149,10 +150,10 @@ public class CustomerGUI {
 		searchBtn = new JButton("Search Inventory");
 		searchBtn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				//get productId from text field
+				//get ProductID from text field
 				
 				try {
-					String userinput = productIDtextField.getText();//get productId from textfield with userinput
+					String userinput = ProductIDtextField.getText();//get ProductID from textfield with userinput
 					
 					List<ProductInfo> productInfo = null;
 					
@@ -161,7 +162,7 @@ public class CustomerGUI {
 						
 					}
 					else {
-						productInfo = productTable.getAllProudctInfo();//if productId empty, get all productinfo
+						productInfo = productTable.getAllProudctInfo();//if ProductID empty, get all productinfo
 					}
 					//prints the data onto the GUI table
 				ProductTableModel model = new ProductTableModel(productInfo);
@@ -197,18 +198,40 @@ public class CustomerGUI {
 		addBtn = new JButton("Order Product");
 		
 		addBtn.addActionListener(new ActionListener() { // connecting method to mysql database with GU to show inventory
-			public void actionPerformed(ActionEvent e) {
-				List<ProductInfo> productInfo = null;
-				try {
+			public void actionPerformed(ActionEvent e) {			
+			
+				double Quantity = Double.valueOf(quantitytextField.getText());				
+				String date = datetextField.getText();
+				String custEmail = custEmailtextfield.getText();
+				double custLocation = Double.valueOf(custLocationtextField.getText());
+				String ProductID = ProductIDtextField.getText();
 				
-				} catch (Exception e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
+				
+				DateTimeFormatter dateFormatter = 
+				        new DateTimeFormatterBuilder()
+				            .parseCaseInsensitive()
+				            .appendPattern("yyyy-MM-dd")
+				            .toFormatter(Locale.ENGLISH);
+				LocalDate date2 = LocalDate.parse(date, dateFormatter);
+				System.out.println("this is quantity = "+Quantity);
+				System.out.println("email="+custEmail);
+				System.out.println("location="+custLocation);
+				System.out.println("producctid="+ProductID);
+				System.out.println("date= "+ date2);
+				
+				
+					
+				try {
+					
+					orderTable.addOrder(date2, custEmail, custLocation, ProductID, Quantity);
+					} catch (Exception e2) {
+						// TODO Auto-generated catch block
+						e2.printStackTrace();
+					}
 			}
-		});
-		addBtn.setBounds(6, 160, 134, 29);
-		frmClass.getContentPane().add(addBtn);
+			});
+			addBtn.setBounds(6, 160, 134, 29);
+			frmClass.getContentPane().add(addBtn);
 		
 		
 		
