2f6c48fdd4c4e4e6f7275b6db8a445d7578856bf
tonezdinny <54297178+tonezdinny@users.noreply.github.com>
2022-04-19 16:34:55 -0600

    Updated Files for the Website

---
+++ SpringBootWebsite/CS3250Project/src/test/java/com/example/CS3250Project/Cs3250ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CS3250Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs3250ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
